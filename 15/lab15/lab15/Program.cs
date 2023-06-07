953098f0b21a6d46de55496ac3e447b858e34a54
ksesha <72476438+crinanis@users.noreply.github.com>
2023-06-07 23:05:13 +0300

    Add files via upload

---
+++ 15/lab15/lab15/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("1. Скрыть текст");
+Console.WriteLine("2. Извлечь текст");
+Console.Write("Выберите действие (1 или 2): ");
+string choice = Console.ReadLine();
+
+if (choice == "1")
+{
+    Console.Write("Введите путь к документу-контейнеру (docx): ");
+    string containerPath = Console.ReadLine();
+    Console.Write("Введите текст, который нужно скрыть: ");
+    string textToHide = Console.ReadLine();
+    Console.Write("Введите путь для сохранения измененного документа (docx): ");
+    string outputPath = Console.ReadLine();
+
+    TextSteganography.HideText(containerPath, textToHide, outputPath);
+    Console.WriteLine("Текст успешно скрыт!");
+}
+else if (choice == "2")
+{
+    Console.Write("Введите путь к документу со скрытым текстом (docx): ");
+    string containerPath = Console.ReadLine();
+    string hiddenText = TextSteganography.ExtractText(containerPath);
+    Console.WriteLine("Скрытый текст: " + hiddenText);
+}
+else
+{
+    Console.WriteLine("Неверный выбор.");
+}
+
+Console.ReadLine();
---
+++ 15/lab15/lab15/TextStenography.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using NPOI.XWPF.UserModel;
+
+public class TextSteganography
+{
+    public static void HideText(string containerPath, string textToHide, string outputPath)
+    {
+        using (FileStream stream = new FileStream(containerPath, FileMode.Open, FileAccess.ReadWrite))
+        {
+            XWPFDocument doc = new XWPFDocument(stream);
+
+            foreach (XWPFParagraph paragraph in doc.Paragraphs)
+            {
+                foreach (XWPFRun run in paragraph.Runs)
+                {
+                    string text = run.Text;
+
+                    string modifiedText = ApplyKerning(text);
+
+                    if (modifiedText != text)
+                    {
+                        run.SetText(modifiedText);
+                        run.FontSize = 14;
+                    }
+                }
+            }
+
+            XWPFParagraph lastParagraph = doc.Paragraphs[doc.Paragraphs.Count - 1];
+            XWPFRun hiddenRun = lastParagraph.InsertNewRun(0);
+            hiddenRun.SetText(textToHide);
+            hiddenRun.FontSize = 1;
+
+            using (FileStream output = new FileStream(outputPath, FileMode.Create))
+            {
+                doc.Write(output);
+            }
+        }
+    }
+
+    public static string ExtractText(string containerPath)
+    {
+        using (FileStream stream = new FileStream(containerPath, FileMode.Open, FileAccess.Read))
+        {
+            XWPFDocument doc = new XWPFDocument(stream);
+
+            StringBuilder hiddenText = new StringBuilder();
+
+            foreach (XWPFParagraph paragraph in doc.Paragraphs)
+            {
+                foreach (XWPFRun run in paragraph.Runs)
+                {
+                    string text = run.Text;
+
+                    if (run.FontSize == 1)
+                    {
+                        hiddenText.Append(text);
+                    }
+                }
+            }
+
+            return hiddenText.ToString();
+        }
+    }
+
+    static string ApplyKerning(string text)
+    {
+        StringBuilder modifiedText = new StringBuilder();
+
+        for (int i = 0; i < text.Length - 1; i++)
+        {
+            char currentChar = text[i];
+            char nextChar = text[i + 1];
+
+            modifiedText.Append(currentChar);
+
+            if (CharUnicodeInfo.GetUnicodeCategory(currentChar) != CharUnicodeInfo.GetUnicodeCategory(nextChar))
+            {
+                modifiedText.Append("\u200A");
+            }
+        }
+
+        modifiedText.Append(text[text.Length - 1]);
+
+        return modifiedText.ToString();
+    }
+}
