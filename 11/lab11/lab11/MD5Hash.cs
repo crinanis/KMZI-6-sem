a45bd94233a16d19399647b2f427f69d299d22eb
ksesha <72476438+crinanis@users.noreply.github.com>
2023-06-07 23:01:55 +0300

    Add files via upload

---
+++ 11/lab11/lab11/MD5Hash.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public class MD5Hash
+{
+    public static string CalculateMD5Hash(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            throw new ArgumentNullException(nameof(input));
+        }
+
+        using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            byte[] hashBytes = new byte[md5.HashSize / 8];
+
+            if (md5.TryComputeHash(inputBytes, hashBytes, out int bytesWritten))
+            {
+                StringBuilder builder = new StringBuilder(hashBytes.Length * 2);
+
+                foreach (byte hashByte in hashBytes)
+                {
+                    builder.AppendFormat("{0:x2}", hashByte);
+                }
+
+                return builder.ToString();
+            }
+            else
+            {
+                throw new InvalidOperationException("Failed to compute MD5 hash.");
+            }
+        }
+    }
+}
---
+++ 11/lab11/lab11/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+string input = "Budanowa Ksenya Andreevna";
+
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+
+string md5Hash = MD5Hash.CalculateMD5Hash(input);
+
+stopwatch.Stop();
+
+Console.WriteLine("Входные данные: " + input);
+Console.WriteLine("MD5 Hash: " + md5Hash);
+Console.WriteLine("Время хэширования {0} символов составило {1} мс ", input.Length ,stopwatch.ElapsedMilliseconds);
